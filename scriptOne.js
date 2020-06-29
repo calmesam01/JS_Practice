507f57a549e96b357ea57569396dbe3ac13a4d46
calmesam01 <sanyamsareen01@gmail.com>
2020-06-29 19:11:10 -0400

    Add js folder

--- scriptOne.js
+++
@@ -1,8 +0,0 @@
-myFriends = ["Sam", "Lav", "Vis", "Kar", "Sat", "Rhy", "Ris", "Kun"];
-var friends = document.getElementById("friends");
-friends.innerHTML = "My Friends are: "
-for (i in myFriends) {
-    var friend = document.createElement("li");
-    friend.innerHTML = myFriends[i];
-    friends.appendChild(friend);
-}
